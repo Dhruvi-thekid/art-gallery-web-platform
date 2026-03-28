c7c5755508ddefa5e23a9039ca9a949411c733b5
Dhruvi <sanganidhruvi2006@gmail.com>
2026-03-28 17:51:47 +0530

    Remove MongoDB connection string from update_form.php

--- update_form.php
+++ update_form.php
@@ -2,7 +2,7 @@
 session_start();
 require 'vendor/autoload.php'; 
 
-$client = new MongoDB\Client("mongodb+srv://sanganidhruvi2006:Sangani.11@cluster0.7ixtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+$client = new MongoDB\Client("#");
 $collection = $client->Mongodb->test; 
 
 $id = $_GET['id'];
